2acb0986fdeca1e6e5099affe20a28eb412a870e
Matthewdewaal554 <ST10232063@vcconnect.edu.za>
2025-08-01 14:01:05 +0200

    Add project files.

---
+++ IndexIceTask/Games.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexIceTask
+{
+
+
+   public class Games
+    {
+        int price, year;
+        string name, genre;
+        public Games(int price, int year, string name, string genre)
+        {
+            this.price = price;
+            this.year = year;
+            this.name = name;
+            this.genre = genre;
+        }
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return price;
+                else if (index == 1)
+                    return year;
+                else if (index == 2)
+                    return name;
+                else if (index == 3)
+                    return genre;
+                else
+                    return null;
+            }
+            set
+            {
+                if (index == 0)
+                    price = (int)value;
+                else if (index == 1)
+                    year = (int)value;
+                else if (index == 2)
+                    name = (string)value;
+                else if (index == 3)
+                    genre = (string)value;
+            }
+        }
+            public object this[string index]
+        {
+            get
+            {
+                if (index == "price")
+                    return price;
+                else if (index == "year")
+                    return year;
+                else if (index == "name")
+                    return name;
+                else if (index == "genre")
+                    return genre;
+                else
+                    return null;
+            }
+            set
+            {
+                if (index == "price")
+                    price = (int)value;
+                else if (index == "year")
+                    year = (int)value;
+                else if (index == "name")
+                    name = (string)value;
+                else if (index == "genre")
+                    genre = (string)value;
+            }
+
+        }
+    } 
+}
+ 
---
+++ TestIndex/Test1.cs
@@ -0,0 +1,45 @@
+﻿namespace TestIndex
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestYear()
+        {
+            IndexIceTask.Games game = new IndexIceTask.Games(259, 2018, "fortnite", "Battleroyal");
+            Assert.AreEqual(2018, game["year"]);
+            Assert.AreEqual(2018, game[1]);
+        }
+        [TestMethod]
+        public void TestName()
+        {
+            IndexIceTask.Games game = new IndexIceTask.Games(12, 2009, "LOL", "MOBA");
+            Assert.AreEqual("LOL", game["name"]);
+            Assert.AreEqual("LOL", game[2]);
+        }
+        [TestMethod]
+        public void TestDescription()
+        {
+            IndexIceTask.Games game = new IndexIceTask.Games(500, 2020, "Call of duty", "Action");
+            Assert.AreEqual("Action", game["genre"]);
+            Assert.AreEqual("Action", game[3]);
+
+        }
+        [TestMethod]
+        public void TestPrice()
+        {
+            IndexIceTask.Games game = new IndexIceTask.Games(500, 2020, "Call of duty", "Action");
+            Assert.AreEqual(500, game["price"]);
+            Assert.AreEqual(500, game[0]);
+        }
+        [TestMethod]
+
+
+        public void TestGenre()
+        {
+            IndexIceTask.Games game = new IndexIceTask.Games(500, 2020, "Call of duty", "Action");
+            Assert.AreEqual("Action", game["genre"]);
+            Assert.AreEqual("Action", game[3]);
+        }
+    }
+}
